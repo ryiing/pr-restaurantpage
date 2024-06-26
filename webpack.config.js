51dd82f71a7c7c93e515a5ab06519030f1dcf95a
ryiing <ryanying1102@gmail.com>
2024-06-26 00:22:18 -0400

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  "mode": 'development',
+  devtool: 'inline-source-map',
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'pr-restaurantpage',
+      filename: 'index.html',
+      inject: 'body',
+      template: 'src/index.html',
+    }),
+  ],
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+};
